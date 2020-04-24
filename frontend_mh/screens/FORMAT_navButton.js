de9f97fa2c715f59c743f823d7250e69bda45616
Samuel Webber <samuelwebber@Samuels-MacBook-Pro.local>
2020-04-24 15:36:44 +0100

    first two pages done, merge

--- frontend_mh/screens/FORMAT_navButton.js
+++ frontend_mh/screens/FORMAT_navButton.js
@@ -6,36 +6,31 @@ const FORMAT_navButton = {
   F_navButton_margin: 10,
   F_navButton_marginTop: "20%",
   F_navButton_marginVertical: 10,
-  F_navButton_alignSelf:"center",
-  F_navButton_alignItems:"center",
+  F_navButton_alignSelf: "center",
+  F_navButton_alignItems: "center",
   F_navButton_flexDirection: "row",
   F_navButton_justifyContent: "space-around",
-  F_navButtonContainer_marginTop:"20%",
+  F_navButtonContainer_marginTop: "20%"
+};
 
-  },
- 
+const FORMAT_navButtonText = {
+  F_navButtonText_padding: 5,
+  F_navButtonText_textAlign: "center",
+  F_navButtonText_justifyContent: "center",
+  F_navButtonText_alignItems: "center",
+  F_navButtonText_width: 70,
+  F_navButtonText_fontSize: 20,
+  F_navButtonText_borderRadius: 5,
+  F_navButtonText_marginVertical: 50,
+  F_navButtonText_color: COLS.C4_DARK_TEXT
+};
 
-  const FORMAT_navButtonText={
-    F_navButtonText_padding:5,
-    F_navButtonText_textAlign:"center",
-    F_navButtonText_justifyContent:"center",
-    F_navButtonText_alignItems:"center",
-    F_navButtonText_width:70,
-    F_navButtonText_fontSize:20,
-    F_navButtonText_borderRadius:5,
-    F_navButtonText_marginVertical: 50,
-    F_navButtonText_color: COLS.C4_DARK_TEXT
-  };
-
-
-  const FORMAT_navButtonBackground={
-F_navButtonBackground_backgroundColor: COLS.C5_LIGHT_TEXT,
-F_navButtonBackground_width: 200,
-F_navButtonBackground_alignSelf: "center",
-F_navButtonBackground_margin: 5,
-F_navButtonBackground_borderRadius: 5
-  };
-
-
+const FORMAT_navButtonBackground = {
+  F_navButtonBackground_backgroundColor: COLS.C5_LIGHT_TEXT,
+  F_navButtonBackground_width: 200,
+  F_navButtonBackground_alignSelf: "center",
+  F_navButtonBackground_margin: 5,
+  F_navButtonBackground_borderRadius: 5
+};
 
 export { FORMAT_navButton, FORMAT_navButtonText, FORMAT_navButtonBackground };
