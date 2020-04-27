98a8fe27687269a1928127ab67e009b86284adb7
Samuel Webber <samuelwebber@Samuels-MacBook-Pro.local>
2020-04-27 17:05:31 +0100

    route tests

--- backend_mh/tests/modelsfoodIntake.test.js
+++ backend_mh/tests/modelsfoodIntake.test.js
@@ -1,52 +1,58 @@
-// /* global jest expect, describe, it */
-// /* eslint-disable no-console */
+/* global jest expect, describe, it */
+/* eslint-disable no-console */
 
-// import {
-//   getFoodIntake,
-//   getFoodIntakeById,
-//   getFoodIntakeByRecipeId,
-//   getFoodIntakeByUserId,
-//   addFoodIntake,
-//   patchFoodIntake,
-// } from "../models/foodIntake";
-// import { isMainThread } from "worker_threads";
+import {
+  getFoodIntake,
+  getFoodIntakeById,
+  getFoodIntakeByRecipeId,
+  getFoodIntakeByUserId,
+  addFoodIntake,
+  patchFoodIntake,
+} from "../models/foodIntake";
+import { isMainThread } from "worker_threads";
 
-// describe(`foodIntake returns getFoodIntake`, async () => {
-//   it(`queries foodIntake sent`, () => {
-//     const mockQuery = jest.fn()
-//     await getFoodIntake(`SELECT * FROM food_intake`)(mockQuery);
-//     //all of the calls the mock function was invoked on
-//     console.log(mockQuery.mock.calls)
-//     expect(mockQuery.mock.calls[0][0]).toEqual({type: })
-//   })
-// it(`queries foodIntake fulfilled`), () => {
-// const mockQuery = jest.fn()
-// const foodIntake = (meal_id,
-//     user_id,
-//     recipe_id,
-//     meal_date_time,
-//     confirmed_eaten,
-//     additional_preferences)
-// => {
-//     if (meal_id.typeof === "string" &&
-//     user_id.typeof === "string" &&
-//     recipe_id ==="string"  &&
-//     meal_date_time ==="number" &&
-//     confirmed_eaten ==="boolean" &&
-//     additional_preferences ==="string"
-//     )
-//     return `foodIntake generating correctly in tables`
-// }
-// throw new Error(`incorrect query for FoodIntake`)
-// }
-// await actions.getFoodIntake("string", "string", "string", "number","boolean","string",mockQuery)
+// async function getFoodIntake() {
+//     const res = await query(`SELECT * FROM food_intake`);
+//     return res.rows;
+//   }
 
-// expect(mockQuery.mock.calls)
+describe(`foodIntake returns getFoodIntake`, async () => {
+  it(`queries foodIntake sent`, () => {
+    const mockQuery = jest.fn()
+    await getFoodIntake(`SELECT * FROM food_intake`)(mockQuery);
+    //all of the calls the mock function was invoked on
+    console.log(mockQuery.mock.calls)
+    expect(mockQuery.mock.calls[0][0]).toEqual({type: })
+  })
 
-//   });
+it(`queries foodIntake fulfilled`), () => {
+const mockQuery = jest.fn()
+const foodIntake = (meal_id,
+    user_id,
+    recipe_id,
+    meal_date_time,
+    confirmed_eaten,
+    additional_preferences)
+=> {
+    if (meal_id.typeof === "string" &&
+    user_id.typeof === "string" &&
+    recipe_id ==="string"  &&
+    meal_date_time ==="number" &&
+    confirmed_eaten ==="boolean" &&
+    additional_preferences ==="string"
+    )
+    return `foodIntake generating correctly in tables`
+}
+throw new Error(`incorrect query for FoodIntake`)
+}
+await actions.getFoodIntake("string", "string", "string", "number","boolean","string",mockQuery)
 
-// describe("foodIntake returns getFoodIntakeById");
-// describe("foodIntake returns foodIntakeByRecipeId");
-// describe("foodIntake returns getFoodIntakeByRecipeId");
-// describe("foodIntake returns getFoodIntakeByUserId");
-// describe("foodIntake returns patchFoodIntake");
+expect(mockQuery.mock.calls)
+
+  });
+
+describe("foodIntake returns getFoodIntakeById");
+describe("foodIntake returns foodIntakeByRecipeId");
+describe("foodIntake returns getFoodIntakeByRecipeId");
+describe("foodIntake returns getFoodIntakeByUserId");
+describe("foodIntake returns patchFoodIntake");
