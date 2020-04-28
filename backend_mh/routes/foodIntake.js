e908420a6f837743b337412dc283ae27d7386698
Murray2015 <murrayhoggett@gmail.com>
2020-04-28 11:14:23 +0100

    Added try catch and lots of if statements to try and avoid silent errors in bad http requests

--- backend_mh/routes/foodIntake.js
+++ backend_mh/routes/foodIntake.js
@@ -9,103 +9,173 @@ const {
 } = require("../models/foodIntake");
 const router = express.Router();
 
+// Gets a specific foodIntake
 router.get("/:foodIntakeId", async (req, res) => {
   const { foodIntakeId } = req.params;
   console.log("Recieved GET request for foodIntakeId: ", foodIntakeId);
-  const data = await getFoodIntakeById(foodIntakeId);
-  console.log("data is:", data);
-  if (data.length > 0) {
-    return res.status(200).json({
-      message: "Received foodIntake Using ID",
-      success: true,
-      payload: data,
+  try {
+    const data = await getFoodIntakeById(foodIntakeId);
+    console.log("data is:", data);
+    if (data.length > 0) {
+      return res.status(200).json({
+        message: "Received foodIntake Using ID",
+        success: true,
+        payload: data,
+      });
+    }
+    console.warn(
+      "Failed getting foodIntake id:",
+      foodIntakeId,
+      "from database."
+    );
+    return res.status(400).json({
+      message: "Failed to fetch from database!",
+      success: false,
+    });
+  } catch (err) {
+    console.warn(
+      "Failed getting foodIntake id:",
+      foodIntakeId,
+      "from database. Got an error:",
+      err
+    );
+    return res.status(400).json({
+      message: "Failed to fetch from database!",
+      success: false,
     });
   }
-  console.warn("Failed getting foodIntake id:", foodIntakeId, "from database.");
-  return res.status(400).json({
-    message: "Failed to fetch from database!",
-    success: false,
-    payload: data,
-  });
 });
 
 router.get("/", async (req, res) => {
-  const { recipe_id, user_id } = req.query;
-  if (recipe_id) {
-    const data = await getFoodIntakeByRecipeId(recipe_id);
-    console.log("data", data, recipe_id);
-    if (data[0]) {
-      return res.status(200).json({
-        message: "foodIntake for recipeId" + recipe_id + " enclosed.",
-        success: true,
-        payload: data,
-      });
+  try {
+    //if recipe undefined will run as below
+    const { recipe_id, user_id } = req.query;
+    if (recipe_id) {
+      const data = await getFoodIntakeByRecipeId(recipe_id);
+      console.log("data", data, recipe_id);
+      if (data[0]) {
+        return res.status(200).json({
+          message: "foodIntake for recipeId" + recipe_id + " enclosed.",
+          success: true,
+          payload: data,
+        });
+      }
+      //else if user id is defined, but recipe undefined, it will run as below
+      return res
+        .status(400)
+        .json({ message: "Failed to access database", success: false });
+    } else if (user_id) {
+      const data = await getFoodIntakeByUserId(user_id);
+      console.log("data", data);
+      if (data[0]) {
+        return res.status(200).json({
+          message: "foodIntake for userId" + user_id + " enclosed.",
+          success: true,
+          payload: data,
+        });
+      }
+      //else if user id is defined, and recipe undefined, it will run as below
+      return res
+        .status(400)
+        .json({ message: "Failed to access database", success: false });
+    } else {
+      console.log("Received GET request for all foodIntake");
+      const data = await getFoodIntake();
+      console.log("data", data);
+      if (data[0]) {
+        return res.status(200).json({
+          message: "All foodIntake enclosed",
+          success: true,
+          payload: data,
+        });
+      }
+      return res
+        .status(400)
+        .json({ message: "Failed to access database", success: false });
     }
+  } catch (err) {
+    console.warn("Error recived:", err);
     return res
       .status(400)
-      .json({ message: "Failed to access database", success: false });
-  } else if (user_id) {
-    const data = await getFoodIntakeByUserId(user_id);
-    console.log("data", data);
-    if (data[0]) {
-      return res.status(200).json({
-        message: "foodIntake for userId" + user_id + " enclosed.",
-        success: true,
-        payload: data,
-      });
-    }
-    return res
-      .status(400)
-      .json({ message: "Failed to access database", success: false });
-  } else {
-    console.log("Received GET request for all foodIntake");
-    const data = await getFoodIntake();
-    console.log("data", data);
-    if (data[0]) {
-      return res.status(200).json({
-        message: "All foodIntake enclosed",
-        success: true,
-        payload: data,
-      });
-    }
-    return res
-      .status(400)
-      .json({ message: "Failed to access database", success: false });
+      .json({ message: "Failed to access database", success: false, err });
   }
 });
 
 // Post request to add/insert new user
 router.post("/", async (req, res) => {
   const { body } = req;
-  console.log("Recieved a POST request to foodIntake", body);
-  const data = await addFoodIntake(body);
-  if (data.rows) {
+  if (!body) {
     return res
-      .status(201)
-      .json({ message: "Inserted new foodIntake", success: true });
+      .status(400)
+      .json({
+        message: "Failed to insert foodIntake. No body found:" + body,
+        success: false,
+      });
   }
-  return res
-    .status(400)
-    .json({ message: "Failed to insert foodIntake", success: false });
+  try {
+    console.log("Recieved a POST request to foodIntake", body);
+    const data = await addFoodIntake(body);
+    if (data.rows) {
+      return res
+        .status(201)
+        .json({ message: "Inserted new foodIntake", success: true });
+    }
+    return res
+      .status(400)
+      .json({ message: "Failed to insert foodIntake", success: false });
+  } catch (err) {
+    console.warn("Got an error in post request. Error:", err);
+    return res
+      .status(400)
+      .json({
+        message: "Failed to insert foodIntake. Got an error.",
+        success: false,
+      });
+  }
 });
 
 // Patch request to update a foodIntake
 router.patch("/:id", async (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  console.log(body);
-  const data = await patchFoodIntake(body, id);
-  console.log(data);
-  if (data) {
-    return res.status(200).json({
-      message: "foodIntake Updated",
-      success: true,
-      payload: data,
-    });
+  if (!id) {
+    return res
+      .status(400)
+      .json({
+        message: "foodIntake Update failed. No id found.",
+        success: false,
+      });
   }
-  return res
-    .status(400)
-    .json({ message: "foodIntake Update failed", success: false });
+  if (!body) {
+    return res
+      .status(400)
+      .json({
+        message: "foodIntake Update failed. No body found.",
+        success: false,
+      });
+  }
+  try {
+    console.log(body);
+    const data = await patchFoodIntake(body, id);
+    console.log(data);
+    if (data) {
+      return res.status(200).json({
+        message: "foodIntake Updated",
+        success: true,
+        payload: data,
+      });
+    }
+    return res
+      .status(400)
+      .json({ message: "foodIntake Update failed", success: false });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({
+        message: "foodIntake Update failed. Got an error",
+        success: false,
+      });
+  }
 });
 
 module.exports = router;
--- backend_mh/routes/users.js
+++ backend_mh/routes/users.js
@@ -9,51 +9,93 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   console.log("Received GET request for all users");
-  const data = await getUsers();
-  console.log("data", data);
-  if (data[0]) {
-    return res.status(200).json({
-      message: "All users enclosed",
-      success: true,
-      payload: data,
-    });
+  try {
+    const data = await getUsers();
+    console.log("data", data);
+    if (data[0]) {
+      return res.status(200).json({
+        message: "All users enclosed",
+        success: true,
+        payload: data,
+      });
+    }
+    console.warn("Failed to access database");
+    return res
+      .status(400)
+      .json({ message: "Failed to access database", success: false });
+  } catch (err) {
+    console.warn("Error recived:", err);
+    return res
+      .status(400)
+      .json({ message: "Failed to access database", success: false });
   }
-  return res
-    .status(400)
-    .json({ message: "Failed to access database", success: false });
 });
 
 router.get("/:userId", async (req, res) => {
   const { userId } = req.params;
   console.log("Recieved GET request for userId: ", userId);
-  const data = await getUserById(userId);
-  if (data.length) {
-    return res.status(200).json({
-      message: "Received User Using ID",
-      success: true,
-      payload: data,
+  try {
+    const data = await getUserById(userId);
+    if (data.length) {
+      return res.status(200).json({
+        message: "Received User Using ID",
+        success: true,
+        payload: data,
+      });
+    }
+    console.warn("Failed getting user id:", userId, "from database.");
+    return res.status(400).json({
+      message: "Failed to fetch from database!",
+      success: false,
+    });
+  } catch (err) {
+    console.warn(
+      "Failed getting user id:",
+      userId,
+      "from database. Error:",
+      err
+    );
+    return res.status(400).json({
+      message: "Failed to fetch from database!",
+      success: false,
     });
   }
-  console.warn("Failed getting user id:", userId, "from database.");
-  return res.status(400).json({
-    message: "Failed to fetch from database!",
-    success: false,
-  });
 });
 
 // Post request to add/insert new user
 router.post("/", async (req, res) => {
   const { body } = req;
-  console.log("Recieved a POST request to users", body);
-  const data = await addUser(body);
-  if (data.rows) {
+  if (!body) {
     return res
-      .status(201)
-      .json({ message: "Inserted new user", success: true });
+      .status(400)
+      .json({
+        message: "Failed to insert user. No body recieved on request.",
+        success: false,
+      });
   }
-  return res
-    .status(400)
-    .json({ message: "Failed to insert user", success: false });
+  console.log("Recieved a POST request to users", body);
+  try {
+    const data = await addUser(body);
+    if (data.rows) {
+      return res
+        .status(201)
+        .json({ message: "Inserted new user", success: true });
+    }
+    console.warn("Failed to insert new user. Request body:", body);
+    return res
+      .status(400)
+      .json({ message: "Failed to insert user", success: false });
+  } catch (err) {
+    console.warn(
+      "Failed to insert new user. Request body:",
+      body,
+      "Error:",
+      err
+    );
+    return res
+      .status(400)
+      .json({ message: "Failed to insert user", success: false });
+  }
 });
 
 // Patch request to update a user
@@ -63,18 +105,31 @@ router.patch("/:id", async (req, res) => {
   if (!id || !body) {
     return res
       .status(400)
-      .json({ message: "User Update failed", success: false });
+      .json({
+        message: "User Update failed, problem with id or body",
+        success: false,
+        id,
+        body,
+      });
   }
   const data = await patchUser(body, id);
   console.log(data);
-  if (data) {
+  try {
+    if (data) {
+      return res
+        .status(200)
+        .json({ message: "User Updated", success: true, payload: data });
+    }
+    console.warn("Failed to update  user. Request body:", body);
     return res
-      .status(200)
-      .json({ message: "User Updated", success: true, payload: data });
+      .status(400)
+      .json({ message: "User Update failed", success: false });
+  } catch (err) {
+    console.warn("Failed to update  user. Request body:", body, "Error:", err);
+    return res
+      .status(400)
+      .json({ message: "User Update failed", success: false });
   }
-  return res
-    .status(400)
-    .json({ message: "User Update failed", success: false });
 });
 
 module.exports = router;
