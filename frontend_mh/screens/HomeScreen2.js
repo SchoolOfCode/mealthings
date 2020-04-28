d50d59d29e0b339edc3cd27329c14e6c861f7217
Murray2015 <murrayhoggett@gmail.com>
2020-04-28 16:57:21 +0100

    Fixed fetches by making them a bit more defensive

--- frontend_mh/screens/HomeScreen2.js
+++ frontend_mh/screens/HomeScreen2.js
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { COLS } from "./COLS";
 import {
   FORMAT_navButtonBackground,
-  FORMAT_navButtonText
+  FORMAT_navButtonText,
 } from "./FORMAT_navButton";
 import { FORMAT_logo } from "./FORMAT_logo";
 import { FORMAT_welcomeContainer } from "./FORMAT_containers";
@@ -39,50 +39,51 @@ export default function HomeScreen2({ navigation }) {
 }
 
 HomeScreen2.navigationOptions = {
-  header: null
+  header: null,
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLS.C_BG,
-    justifyContent: "center"
+    justifyContent: "center",
   },
-  mealThingsLogo: {
-    alignItems: FORMAT_logo.F_logo_alignItems,
-    margin: FORMAT_logo.F_logo_margin,
-    justifyContent: FORMAT_logo.F_logo_justifyContent
+  welcomeContainer: {
+    alignItems: FORMAT_welcomeContainer.F_welcomeContainer_alignItems,
+    marginTop: FORMAT_welcomeContainer.F_welcomeContainer_marginTop,
+    marginBottom: FORMAT_welcomeContainer.F_welcomeContainer_marginBottom,
   },
   logoCircle: {
     width: FORMAT_logo.F_logoCircle_width,
     height: FORMAT_logo.F_logoCircle_height,
     borderRadius: FORMAT_logo.F_logoCircle_borderRadius,
     backgroundColor: COLS.C_LOGO_BG,
-    marginBottom: 15
+    marginBottom: 15,
   },
+  mealThingsLogo: {
+    alignItems: FORMAT_logo.F_logo_alignItems,
+    margin: FORMAT_logo.F_logo_margin,
+    justifyContent: FORMAT_logo.F_logo_justifyContent,
+  },
+
   tagLine: {
     color: COLS.C5_LIGHT_TEXT,
-    fontSize: FORMAT_tagLine.F_tagLine_fontSize
+    fontSize: FORMAT_tagLine.F_tagLine_fontSize,
   },
   buttonContainer: {
-    marginTop: "20%"
+    marginTop: "20%",
   },
   buttonBackground: {
     backgroundColor: COLS.C5_LIGHT_TEXT,
     width: FORMAT_navButtonBackground.F_navButtonBackground_width,
     alignSelf: FORMAT_navButtonBackground.F_navButtonBackground_alignSelf,
     margin: FORMAT_navButtonBackground.F_navButtonBackground_margin,
-    borderRadius: FORMAT_navButtonBackground.F_navButtonBackground_borderRadius
+    borderRadius: FORMAT_navButtonBackground.F_navButtonBackground_borderRadius,
   },
   buttonText: {
     color: COLS.C4_DARK_TEXT,
     textAlign: FORMAT_navButtonText.F_navButtonText_textAlign,
     padding: FORMAT_navButtonText.F_navButtonText_padding,
-    fontSize: FORMAT_navButtonText.F_navButtonText_fontSize
+    fontSize: FORMAT_navButtonText.F_navButtonText_fontSize,
   },
-  welcomeContainer: {
-    alignItems: FORMAT_welcomeContainer.F_welcomeContainer_alignItems,
-    marginTop: FORMAT_welcomeContainer.F_welcomeContainer_marginTop,
-    marginBottom: FORMAT_welcomeContainer.F_welcomeContainer_marginBottom
-  }
 });
--- frontend_mh/screens/Loginpage.js
+++ frontend_mh/screens/Loginpage.js
@@ -7,14 +7,14 @@ import {
   View,
   TextInput,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import { COLS } from "./COLS";
 import { FORMAT_background } from "./FORMAT_background";
 import {
   FORMAT_containers,
   FORMAT_welcomeContainer,
-  FORMAT_moreChoicesContainer
+  FORMAT_moreChoicesContainer,
 } from "./FORMAT_containers";
 import {
   FORMAT_switches,
@@ -25,7 +25,7 @@ import {
   FORMAT_arrow,
   FORMAT_icons,
   FORMAT_mainRecipe,
-  FORMAT_tagLine
+  FORMAT_tagLine,
 } from "./FORMAT_extraComponents";
 import { FORMAT_headings, FORMAT_textBoxHeading } from "./FORMAT_headings";
 import { FORMAT_images } from "./FORMAT_images";
@@ -34,7 +34,7 @@ import { FORMAT_logo } from "./FORMAT_logo";
 import {
   FORMAT_navButton,
   FORMAT_navButtonText,
-  FORMAT_navButtonBackground
+  FORMAT_navButtonBackground,
 } from "./FORMAT_navButton";
 import { FORMAT_text, FORMAT_fonts } from "./FORMAT_text";
 
@@ -45,9 +45,11 @@ export default function Loginpage({ navigation }) {
   function tracked(enteredText) {
     setName(enteredText);
   }
+
   function tracker(enteredText) {
     setPassword(enteredText);
   }
+
   function handleSubmit() {
     // TODO - This should not be a POST, but should do a get request with the username (use the query string route), and check if the password the user entered matches the password that the user entered. If the password is wrong the user should get an error message and another change to enter their password. If the password is correct they should be routed to the Landing Page screen.
 
@@ -71,67 +73,66 @@ export default function Loginpage({ navigation }) {
     //   });
     navigation.navigate("LandingPage");
   }
+  console.log("Hi from login page! ");
+
   return (
     <View style={styles.container}>
-      <View style={styles.border}>
-        <View style={styles.positioning}>
-          <View style={styles.logoCircle}>
-            <Image source={require("../assets/images/Mealthings.png")} />
-          </View>
-          <View>
-            <Text style={styles.tagLine}>Eat Well. Feel Amazing.</Text>
-          </View>
+      <View style={styles.welcomeContainer}>
+        <View style={styles.logoCircle}>
+          <Image source={require("../assets/images/Mealthings.png")} />
         </View>
-        <TextInput
-          style={styles.inputField}
-          placeholder=" enter username"
-          value={name}
-          onChangeText={tracked}
-          placeholderTextColor="white"
-        />
-        <TextInput
-          style={styles.inputField}
-          placeholder=" enter password"
-          value={password}
-          onChangeText={tracker}
-          placeholderTextColor="white"
-        />
-
-        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-          <Text>Login</Text>
-        </TouchableOpacity>
+        <View>
+          <Text style={styles.tagLine}>Eat Well. Feel Amazing.</Text>
+        </View>
       </View>
+      <TextInput
+        style={styles.inputField}
+        placeholder=" enter username"
+        value={name}
+        onChangeText={tracked}
+        placeholderTextColor="white"
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder=" enter password"
+        value={password}
+        onChangeText={tracker}
+        placeholderTextColor="white"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  border: {
-    marginVertical: 50
-  },
   container: {
+    flex: 1,
     backgroundColor: COLS.C_BG,
-    height: FORMAT_containers.F_containerHeaders_height
+    justifyContent: "center",
+  },
+  welcomeContainer: {
+    alignItems: FORMAT_welcomeContainer.F_welcomeContainer_alignItems,
+    marginTop: FORMAT_welcomeContainer.F_welcomeContainer_marginTop,
+    marginBottom: FORMAT_welcomeContainer.F_welcomeContainer_marginBottom,
   },
   mealThingsLogo: {
     alignItems: FORMAT_logo.F_logo_alignItems,
     margin: FORMAT_logo.F_logo_margin,
-    justifyContent: FORMAT_logo.F_logo_justifyContent
+    justifyContent: FORMAT_logo.F_logo_justifyContent,
   },
   logoCircle: {
     width: FORMAT_logo.F_logoCircle_width,
     height: FORMAT_logo.F_logoCircle_height,
     borderRadius: FORMAT_logo.F_logoCircle_borderRadius,
     backgroundColor: COLS.C_LOGO_BG,
-    marginBottom: FORMAT_logo.F_logoCircle_marginBottom
+    marginBottom: 15,
   },
   tagLine: {
     color: COLS.C5_LIGHT_TEXT,
-    left: -10,
     marginBottom: 60,
-    fontSize: FORMAT_tagLine.F_tagLine_fontSize
-  },
-  positioning: {
-    left: 100
+    fontSize: FORMAT_tagLine.F_tagLine_fontSize,
   },
   inputField: {
     marginVertical: FORMAT_inputField.F_inputField_marginVertical,
@@ -140,13 +141,13 @@ const styles = StyleSheet.create({
     alignSelf: FORMAT_inputField.F_inputField_alignSelf,
     alignItems: FORMAT_inputField.F_inputField_alignItems,
     height: FORMAT_inputField.F_inputField_height,
-    borderRadius: FORMAT_inputField.F_inputField_borderRadius
+    borderRadius: FORMAT_inputField.F_inputField_borderRadius,
   },
   button: {
     alignSelf: FORMAT_navButton.F_navButton_alignSelf,
     padding: FORMAT_navButton.F_navButton_padding,
     backgroundColor: COLS.C5_LIGHT_TEXT,
     borderRadius: FORMAT_navButton.F_navButton_borderRadius,
-    marginVertical: 20
-  }
+    marginVertical: 20,
+  },
 });
