4e864706f77e17eb79a4083928070b872f252bec
Murray Hoggett <murrayhoggett@gmail.com>
2020-05-01 11:33:19 +0100

    Delete index.js

--- backend_mh/config/index.js
+++
@@ -1,7 +0,0 @@
-const JWT_SECRET = process.env.JWT_SECRET || "murray_and_hannahs_secret";
-const MEALTHINGS_GMAIL_PASSWORD = process.env.MEALTHINGS_GMAIL_PASSWORD || null;
-
-module.exports = {
-  JWT_SECRET,
-  MEALTHINGS_GMAIL_PASSWORD,
-};
