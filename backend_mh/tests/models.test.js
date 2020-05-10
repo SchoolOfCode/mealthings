f07dbaf78169ed9d5f618fbc29c1a2c1ead90473
Murray2015 <murrayhoggett@gmail.com>
2020-05-10 16:48:47 +0100

    First set of user route tests are done

--- backend_mh/tests/models.test.js
+++ backend_mh/tests/models.test.js
@@ -17,22 +17,50 @@ function isJsonString(str) {
   try {
     JSON.parse(str);
   } catch (e) {
+    console.log("Got an error!", e);
     return false;
   }
   return true;
 }
 
+let user2;
+
+beforeAll(async () => {
+  user2 = await getUserById(2);
+});
+
 describe("User models ", () => {
-  test("query all, response should be JSON", async () => {
+  test("query all, should have at least 3 items in database", async () => {
     const allUsers = await getUsers();
-    const allUsersIsJson = isJsonString(allUsers);
-    expect(allUsersIsJson).toBe(true);
+    expect(allUsers.length).toBeGreaterThanOrEqual(3);
   });
 });
 
-/* 
+describe.each([
+  ["user_id", 2],
+  ["name", "anya"],
+  ["height", 182],
+  ["email_address", "anyaforward@gmail.com"],
+  ["username", "anyaforward"],
+  ["weight", 55],
+  ["weight", 55],
+  ["new_mum", false],
+  ["food_prefs_inc", "beer"],
+  ["food_prefs_exc", "celiac"],
+  ["goals", "lose weight"],
+  ["gender", "Female"],
+])("User 2", (key, expected) => {
+  test(`${key} should be ${expected}`, () => {
+    // console.log(user2);
+    expect(user2[0][key]).toBe(expected);
+  });
+});
 
-Query all users, response should be JSON
+test("User 2 password should be hashed, so should not be password", () => {
+  expect(user2[0].password).not.toEqual("password");
+});
+
+/* 
 Query all users, first line should equal first line of dummy data  // But check this incase they come in a random order!
 Query all users, somewhere within should be all 3 names in dummy 
 Query all users, somewhere within should be all 3 email address
